512506a95d08f694e7a84262d49c33f30c09c4bd
ctsictai <ctsictai@gmail.com>
2021-10-28 18:44:10 +0900

    table_model 추가

--- board/models.py
+++ board/models.py
@@ -1,2 +1,25 @@
 from django.db import models
 
+
+class Board(models.Model):
+    """
+    질문 테이블
+    """
+
+    title = models.CharField(max_length=200,)
+    context = models.TextField(blank=True, null=True)
+    user = models.ForeignKey("user.User", models.DO_NOTHING, related_name="board_user")
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    """
+    답변 테이블
+    """
+
+    context = models.CharField(max_length=500,)
+    user = models.ForeignKey("user.User", models.DO_NOTHING, related_name="comment_user")
+    board = models.ForeignKey(Board, models.DO_NOTHING, related_name="comment_board")
+
--- user/models.py
+++ user/models.py
@@ -1,2 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.utils import timezone
 
+
+class UserManager(BaseUserManager):
+    """
+    Customized BaseUserManager
+    """
+
+    def _create_user(self, identification, password, **extra_fields):
+        if not identification:
+            raise ValueError("The given identification must be set")
+        identification = self.identification
+        user = self.model(identification=identification, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, identification, password, **extra_fields):
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(identification, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    """
+    Customized AbstractBaseUser
+    """
+
+    identification = models.CharField(max_length=30,)
+    username = models.CharField(max_length=30,)
+
+    is_active = models.BooleanField(("active"), default=True,)
+    date_joined = models.DateTimeField(("date joined"), default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "identification"
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
+    def __str__(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.identification
